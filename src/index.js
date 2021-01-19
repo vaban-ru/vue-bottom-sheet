08b1bf66fdfc8a0c93b9fd0efe9d6c8d4afcaa95
Vladimir Banduristov <hello@webzlodimir.ru>
2021-01-19 20:42:51 +0300

    Перевел README, переделал установку компонента

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import VueBottomSheet from './components/VueBottomSheet.vue'
 
-export { VueBottomSheet }
+// Объявление функции установки, выполняемой Vue.use()
+export function install(Vue) {
+    if (install.installed) return;
+    install.installed = true;
+    Vue.component('vue-bottom-sheet', VueBottomSheet);
+}
+
+// Создание значения модуля для Vue.use()
+const plugin = {
+    install
+};
+
+// Автоматическая установка, когда vue найден (например в браузере с помощью тега <script>)
+let GlobalVue = null;
+if (typeof window !== 'undefined') {
+    GlobalVue = window.Vue;
+} else if (typeof global !== 'undefined') {
+    GlobalVue = global.Vue;
+}
+if (GlobalVue) {
+    GlobalVue.use(plugin);
+}
+
+// Экспорт компонента, для использования в качестве модуля (npm/webpack/etc.)
+export default VueBottomSheet;
